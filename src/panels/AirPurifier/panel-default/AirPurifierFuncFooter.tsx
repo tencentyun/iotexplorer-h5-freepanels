e1cdba4cc3cb4a105bb08018a1006db99fef8e7f
v_vjiahuang <v_vjiahuang@tencent.com>
2021-01-06 16:28:05 +0800

    feat(airpurifier): 添加空气净化器免开发面板

---
+++ src/panels/AirPurifier/panel-default/AirPurifierFuncFooter.tsx
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+import * as freePanelIcons from '@icons/device/freePanel';
+import './AirPurifierFuncFooter.less';
+import { AirPurifierSetting } from './AirPurifierSetting';
+import * as wxlib from '@wxlib';
+import { FooterBtnConfig, PageFooter } from "@components/PageFooter";
+import { useHistory } from "react-router-dom";
+
+export const getStatusStr = (powerOff: boolean): string => (!powerOff ? '开启' : '关闭');
+
+export interface FuncFooterProps {
+    offline?: boolean;
+    powerOff?: boolean;
+    controlDeviceData: (id, value) => any;
+    darkMode?: boolean;
+    onSwitchChange?: () => any;
+    isShareDevice?: boolean;
+    templateMap?: any;
+    deviceData?: any;
+}
+
+/**
+ * 按钮本身只有两种状态，actived不透明，默认带半透明
+ *
+ * 定时
+ *
+ * @param btnConfig
+ * @constructor
+ */
+export function AirPurifierFuncFooter({
+    offline,
+    powerOff,
+    controlDeviceData,
+    darkMode,
+    onSwitchChange,
+    isShareDevice,
+    templateMap,
+    deviceData,
+}: FuncFooterProps) {
+    const history = useHistory();
+    const [airPuiifierSettingVisible, setAirPuiifierSettingVisible] = useState(false);
+    const [identifier, setIdentifier] = useState('');
+    const [identifierValue, setIdentifierValue] = useState(null);
+
+
+    const getBtnConfig = (): FooterBtnConfig[] => {
+        const itemClassName = darkMode ? 'dark-mode' : '';
+
+        const configMap: {
+            switch: FooterBtnConfig;
+            timing: FooterBtnConfig;
+            countdown: FooterBtnConfig;
+            more: FooterBtnConfig;
+        } = {
+            switch: {
+                text: '开启',
+                disabled: offline,
+                className: itemClassName,
+                onClick: onSwitchChange ? onSwitchChange : () => controlDeviceData('power_switch', powerOff ? 1 : 0),
+            },
+            timing: {
+                text: '风速',
+                disabled: offline || powerOff,
+                btnClassName: isShareDevice ? 'disabled' : '',
+                className: itemClassName,
+                onClick: () => {
+                    setIdentifier('windspeed');
+                    setIdentifierValue(deviceData.windspeed);
+                    setAirPuiifierSettingVisible(true);
+                },
+            },
+            countdown: {
+                text: '模式',
+                disabled: offline || powerOff,
+                className: itemClassName,
+                onClick: () => {
+                    setIdentifier('mode');
+                    setIdentifierValue(deviceData.mode);
+                    setAirPuiifierSettingVisible(true);
+                }
+            },
+            more: {
+                text: '更多',
+                disabled: offline || powerOff,
+                className: itemClassName,
+                onClick: () => {
+                    history.push('/filter-reset');
+                },
+            },
+        };
+
+        if (darkMode) {
+            if (!offline && !powerOff) {
+                configMap.switch.actived = true;
+            }
+        } else {
+            configMap.switch.actived = !offline;
+        }
+
+        if (!powerOff) {
+            configMap.timing.icon = freePanelIcons.iconTiming;
+            configMap.switch.icon = darkMode ? freePanelIcons.iconSwitch : freePanelIcons.iconSwitchOpen;
+            configMap.countdown.icon = freePanelIcons.iconCountdown;
+            configMap.more.icon = freePanelIcons.iconCountdown;
+        } else {
+            configMap.timing.icon = freePanelIcons.iconTiming;
+
+            if (darkMode) {
+                configMap.switch.icon = freePanelIcons.iconSwitch;
+            } else {
+                configMap.switch.icon = !offline ? freePanelIcons.iconSwitchClose : freePanelIcons.iconSwitch;
+            }
+
+            configMap.switch.text = '关闭';
+            configMap.countdown.icon = freePanelIcons.iconCountdown;
+            configMap.more.icon = freePanelIcons.iconCountdown;
+        }
+
+        return [configMap.switch, configMap.timing, configMap.countdown, configMap.more];
+    };
+
+    return (
+        <>
+            <div className="air-purifier-func-footer">
+                <PageFooter
+                    footerConfig={getBtnConfig()}
+                />
+            </div>
+
+            {airPuiifierSettingVisible && (
+                <AirPurifierSetting
+                    visible={true}
+                    onClose={() => setAirPuiifierSettingVisible(false)}
+                    controlDeviceData={controlDeviceData}
+                    identifier={identifier}
+                    identifierValue={identifierValue}
+                    templateMap={templateMap}
+                />
+            )}
+        </>
+    );
+}
---
+++ src/panels/AirPurifier/panel-default/AirPurifierPanel.tsx
@@ -0,0 +1,156 @@
+import React, { useState } from 'react';
+import {
+	iconSocket,
+} from '@icons/device/freePanel';
+import classNames from 'classnames';
+import { PanelMoreBtn } from '@components/PanelMoreBtn';
+import { BorderSwitch } from "./BorderSwitch";
+import { AirPurifierPanelLayout } from './AirPurifierPanelLayout';
+import { PanelComponentProps } from "@src/entryWrap";
+import './AirPurifierPanel.less';
+
+export function AirPurifierPanel({
+	deviceInfo,
+	deviceData,
+	offline,
+	powerOff,
+	templateMap,
+	doControlDeviceData,
+	onGoTimingProject,
+	onGoDeviceDetail,
+}: PanelComponentProps) {
+	console.log('********', deviceInfo, deviceData, templateMap, offline, powerOff)
+	const airPurifierDisabled = offline || powerOff;
+	const [wetEnable, setWetdownEnable] = useState(deviceData.wet ? true : false);
+	const [anionEnable, setAniondownEnable] = useState(deviceData.anion ? true : false);
+	const [uvEnable, setUvdownEnable] = useState(deviceData.uv ? true : false);
+
+	const transformShow = (val, defalut) => {
+		if (airPurifierDisabled) {
+			return '-'
+		} else {
+			if (deviceData[val] || deviceData[val] === 0) {
+				return deviceData[val] + (templateMap[val].define.unit === 'day' ? '天' : templateMap[val].define.unit);
+			} else {
+				if (deviceInfo.isVirtualDevice) {
+					return defalut;
+				} else {
+					return '-';
+				}
+			}
+		}
+	};
+
+	return (
+		<AirPurifierPanelLayout
+			className={classNames('free-air-purifier-page', {
+				'power-off': powerOff,
+			})}
+			title={deviceInfo.displayName}
+			doControlDeviceData={doControlDeviceData}
+			offline={offline}
+			powerOff={powerOff}
+			deviceData={deviceData}
+			onGoTimingProject={onGoTimingProject}
+			templateMap={templateMap}
+		>
+			<PanelMoreBtn
+				onClick={onGoDeviceDetail}
+				theme='dark'
+			/>
+			<div className="air-purifier-container">
+				<div className='air-purifier-body'>
+					{
+						!airPurifierDisabled && (
+							<div className='mode-speed'>
+								<span className='mode'>模式：{(deviceData.mode || deviceData.mode === 0) ? templateMap.mode.define.mapping[deviceData.mode] : '自动'}</span>
+								<span>风速：{(deviceData.windspeed || deviceData.windspeed === 0) ? templateMap.windspeed.define.mapping[deviceData.windspeed] : '高档'}</span>
+							</div>
+						)
+					}
+					<div className='air-circle'>
+						{
+							airPurifierDisabled ? (
+								<div className="close-down">{offline ? '已离线' : '已关机'}</div>
+							) : (
+									<div>
+										<div className='pm'>PM2.5</div>
+										<div className='pm-value'>{deviceData.pm2_5 ? deviceData.pm2_5 : '300'}</div>
+										<div>室内空气质量：{(deviceData.pm2_5level) ? templateMap.pm2_5level.define.mapping[deviceData.pm2_5level] : '优'}</div>
+									</div>
+
+								)
+						}
+					</div>
+					<div className='air-purifier-property-body'>
+						<div className="air-purifier-property">
+							<div className="label">TVOC</div>
+							<div className="value">{transformShow('tvoc', '1ppm')}</div>
+						</div>
+						<div className="air-purifier-property">
+							<div className="label">湿度</div>
+							<div className="value">{transformShow('humidity', '50%')}</div>
+						</div>
+						<div className="air-purifier-property">
+							<div className="label">温度</div>
+							<div className="value">{transformShow('temperature', '22℃')}</div>
+						</div>
+					</div>
+					<div className='air-purifier-property-body'>
+						<div className="air-purifier-property">
+							<div className="label">滤芯剩余寿命</div>
+							<div className="value">{transformShow('filter_left_level', '70%')}</div>
+						</div>
+						<div className="air-purifier-property">
+							<div className="label">滤芯剩余天数</div>
+							<div className="value">{transformShow('filter_left_days', '200天')}</div>
+						</div>
+						<div className="air-purifier-property">
+							<div className="label">滤芯已使用</div>
+							<div className="value">{transformShow('filter_used_days', '22天')}</div>
+						</div>
+					</div>
+
+					<div className='air-purifier-switch-group'>
+						<div>
+							<span className='air-purifier-switch-label'>加湿</span>
+							<BorderSwitch
+								checked={wetEnable}
+								onChange={(val) => {
+									setWetdownEnable(val);
+									doControlDeviceData('wet', val ? 1 : 0);
+								}}
+								className='air-purifier-switch'
+								disabled={airPurifierDisabled}
+							/>
+						</div>
+						<div>
+							<span className='air-purifier-switch-label'>负离子</span>
+							<BorderSwitch
+								checked={anionEnable}
+								onChange={(val) => {
+									setAniondownEnable(val);
+									doControlDeviceData('anion', val ? 1 : 0);
+								}}
+								className='air-purifier-switch'
+								disabled={airPurifierDisabled}
+							/>
+						</div>
+						<div>
+							<span className='air-purifier-switch-label'>UV杀菌</span>
+							<BorderSwitch
+								checked={uvEnable}
+								onChange={(val) => {
+									setUvdownEnable(val);
+									doControlDeviceData('uv', val ? 1 : 0);
+								}}
+								className='air-purifier-switch'
+								disabled={airPurifierDisabled}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</AirPurifierPanelLayout>
+	);
+}
