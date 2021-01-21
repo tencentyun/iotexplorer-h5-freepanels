9cf77267d6ecf7a699c1cb7a872a94674acc4f22
vinsonxiao(肖宇泽) <vinsonxiao@tencent.com>
2021-01-21 15:45:25 +0800

    feat(mock): 支持模拟设备

--- src/libs/wxlib/router.ts
+++ src/libs/wxlib/router.ts
@@ -7,80 +7,76 @@ import _ from '@underscore';
 import { appendParams } from '@utillib';
 
 export default {
-	/**
-	 * 页面跳转
-	 *
-	 * 默认在当前页面至上跳转
-	 *
-	 * 如传redirect=true,则关闭当前页面跳转
-	 * 如传relaunch=true,则关闭所有页面并跳转
-	 *
-	 * @param url
-	 * @param [waitForResponse]
-	 * @param [redirect]
-	 * @param [relaunch]
-	 * @param [replace] alias for redirect
-	 * @param [data]
-	 * @return {*}
-	 */
-	async go(url: string, {
-		redirect = false,
-		relaunch = false,
-		replace,
-		// 如果需要等待响应，需要自行在目标页面onUnload 时trigger回调
-		waitForResponse,
-		...data
-	}: {
-		redirect?: boolean;
-		relaunch?: boolean;
-		replace?: boolean;
-		// 如果需要等待响应，需要自行在目标页面onUnload 时trigger回调
-		waitForResponse?: boolean;
-		[propName: string]: any;
-	} = {}) {
-		if (typeof replace !== 'undefined') {
-			redirect = replace;
-		}
+  /**
+   * 页面跳转
+   *
+   * 默认在当前页面至上跳转
+   *
+   * 如传redirect=true,则关闭当前页面跳转
+   * 如传relaunch=true,则关闭所有页面并跳转
+   *
+   * @param url
+   * @param [waitForResponse]
+   * @param [redirect]
+   * @param [relaunch]
+   * @param [replace] alias for redirect
+   * @param [data]
+   * @return {*}
+   */
+  async go(url: string, {
+    redirect = false,
+    relaunch = false,
+    replace,
+    // 如果需要等待响应，需要自行在目标页面onUnload 时trigger回调
+    waitForResponse,
+    ...data
+  }: {
+    redirect?: boolean;
+    relaunch?: boolean;
+    replace?: boolean;
+    // 如果需要等待响应，需要自行在目标页面onUnload 时trigger回调
+    waitForResponse?: boolean;
+    [propName: string]: any;
+  } = {}) {
+    if (typeof replace !== 'undefined') {
+      redirect = replace;
+    }
 
-		if (data) {
-			url = appendParams(url, data);
-		}
+    if (data) {
+      url = appendParams(url, data);
+    }
 
-		if (url.indexOf('/pages/Index/Index') > -1) {
-			await wx.miniProgram.reLaunch({ url });
-		} else if (redirect) {
-			await wx.miniProgram.redirectTo({ url });
-		} else if (relaunch) {
-			if (wx.miniProgram.reLaunch) {
-				await wx.miniProgram.reLaunch({ url });
-			} else {
-				await wx.miniProgram.redirectTo({ url });
-			}
-		} else {
-			await wx.miniProgram.navigateTo({ url });
-		}
-	},
+    if (url.indexOf('/pages/Index/Index') > -1) {
+      await sdk.callMpApi('reLaunch', { url });
+    } else if (redirect) {
+      await sdk.callMpApi('redirectTo', { url });
+    } else if (relaunch) {
+      await sdk.callMpApi('reLaunch', { url });
+    } else {
+      await sdk.callMpApi('navigateTo', { url });
+    }
+  },
 
-	replace(url, data = {}) {
-		return this.go(url, { redirect: true, ...data });
-	},
+  replace(url, data = {}) {
+    return this.go(url, { redirect: true, ...data });
+  },
 
-	relaunch(url, data = {}) {
-		return this.go(url, { relaunch: true, ...data });
-	},
+  relaunch(url, data = {}) {
+    return this.go(url, { relaunch: true, ...data });
+  },
 
-	async back(delta: number | { reload?: boolean; params?: any } = 1, { reload = false }: { reload?: boolean } = {}) {
-		console.log('back params', delta, reload);
+  async back(delta: number | { reload?: boolean; params?: any } = 1, { reload = false }: { reload?: boolean } = {}) {
+    console.log('back params', delta, reload);
 
-		if (typeof delta !== 'number' && _.isPlainObject(delta)) {
-			reload = <boolean>delta.reload;
-			delta = 1;
-		}
+    if (typeof delta !== 'number' && _.isPlainObject(delta)) {
+      reload = <boolean>delta.reload;
+      delta = 1;
+    }
 
-		if (reload) {
-			sdk.reloadAfterUnmount();
-		}
+    if (reload) {
+      sdk.reloadAfterUnmount();
+    }
 
-		wx.miniProgram.navigateBack({ delta: <number>delta });
-	},
+    sdk.callMpApi('navigateBack', { delta: <number>delta });
+  },
 };
