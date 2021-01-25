e5bf572292c17a1ba471c143f018e6e8503e5287
vinsonxiao(肖宇泽) <vinsonxiao@tencent.com>
2021-01-25 17:51:55 +0800

    feat(webpack): 调整构建模式

---
+++ webpack/panel-conf.js
@@ -0,0 +1,32 @@
+module.exports = {
+  AirPurifier: {
+    enable: true,
+    panels: [
+      ['panel-default', { enable: true }]
+    ],
+  },
+  FiveRoadHub: {
+    enable: true,
+    panels: [
+      ['panel-default', { enable: true }],
+    ],
+  },
+  FiveRoadLight: {
+    enable: true,
+    panels: [
+      ['panel-default', { enable: true }],
+    ],
+  },
+  Locator: {
+    enable: true,
+    panels: [
+      ['panel-default', { enable: true }],
+    ],
+  },
+  Socket: {
+    enable: true,
+    panels: [
+      ['panel-default', { enable: true }],
+    ],
+  },
+};
