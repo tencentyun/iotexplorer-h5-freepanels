30b4d33c4ba58ae6bfe6bcd5f7381999a1e20591
shuaisguo <shuaisguo@tencent.com>
2022-04-01 15:15:45 +0800

    feat: 支持跳转到视频监控面板

--- src/panels/SmartLocksTwo/Common/Common.tsx
+++ src/panels/SmartLocksTwo/Common/Common.tsx
@@ -4,7 +4,6 @@ import { Home } from './Home';
 import { Log } from './Log';
 import { Settings, CameraConfig, VideoConfig } from './Settings';
 import { Users, UserEdit, PasswordAdd, PasswordResult } from './Users';
-import { VideoMonitor } from './VideoMonitor';
 import { VideoHistory } from './VideoHistory';
 import { EventPreview } from './EventPreview';
 import { TempPassword, AddTempPassword } from './TempPassword';
@@ -21,7 +20,6 @@ const App = () => {
     { path: '/users/psdresult', Component: PasswordResult },
     { path: '/temp/password/index', Component: TempPassword },
     { path: '/temp/password/add', Component: AddTempPassword },
-    { path: '/video/monitor', Component: VideoMonitor },
     { path: '/video/history', Component: VideoHistory },
     { path: '/event/preview', Component: EventPreview },
   ];
