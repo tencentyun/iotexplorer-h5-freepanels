df71ba31425c61508716a7600e681426cde71eff
appbir <appbir@163.com>
2022-06-14 21:52:55 +0800

    冷暖灯倒计时进入刷新

--- src/panels/WhiteLampTwo/Common/CountDown/CountDown.tsx
+++ src/panels/WhiteLampTwo/Common/CountDown/CountDown.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { TimePickerList } from '@custom/TimePickerList';
-
+import { useDeviceInfo } from '@hooks/useDeviceInfo';
 export const CountDown = (props) => {
-  const { deviceData, doControlDeviceData } = props;
+  // const { deviceData, doControlDeviceData } = props;
+  const { doControlDeviceData } = props;
+  // 添加进入刷新定时值
+  const [state] = useDeviceInfo();
   const onChange = (count_down) => {
     doControlDeviceData({ count_down });
   };
@@ -10,7 +13,7 @@ export const CountDown = (props) => {
   return (
     <div>
       <TimePickerList
-        value={deviceData.count_down}
+        value={state.deviceData.count_down}
         onChange={onChange}
         emptyTitle="设置倒计时关灯时间"
       ></TimePickerList>
