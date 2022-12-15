8d59b06b761f445a4456cf46a1a381499bcbb32f
chenwei9 <chenwei9@ruijie.com.cn>
2022-12-15 21:42:44 +0800

    添加电动窗帘purple主题

---
+++ src/panels/ElectricCurtainsV3/panel-qualityPurple/Common.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Router } from '@router';
+import { Home } from './Home/Home';
+import { More } from '../Common/Home/More';
+import { TimerList, TimerAdd, Repeat } from '../Common/Timer';
+
+const App = () => {
+  const route = [
+    { path: '/home', Component: Home },
+    { path: '/more', Component: More },
+    { path: '/timer/list', Component: TimerList },
+    { path: '/timer/add', Component: TimerAdd },
+    { path: '/timer/action/repeat', Component: Repeat },
+  ];
+  return <Router route={route} />;
+};
+
+export default App;
