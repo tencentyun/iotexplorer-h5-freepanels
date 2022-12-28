2319595aec0e0f353456cfc450636db49b41cb54
chenwei9 <chenwei9@ruijie.com.cn>
2022-12-29 02:24:17 +0800

    增加电风扇精品紫主题

--- src/panels/ElectricFanV3/panel-qualityPurple/Home/Home.tsx
+++ src/panels/ElectricFanV3/panel-qualityPurple/Home/Home.tsx
@@ -1,14 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import { Icon } from '@custom/Icon';
-import { Battery } from '@custom/Battery';
-import { OptionDialog } from '@custom/OptionDialog';
 import { Cell } from '@custom/Cell';
 import { CountDown } from '../../Common/CountDown';
+import { Slider } from '@custom/Slider';
+import { OptionDialog } from '@custom/OptionDialog';
 
-
-const MODE_LIST = ['默认', '睡眠模式', '强劲模式', '低速模式'];
-
+const MODE_LIST = ['默认模式', '左右摇头', '上下摇头', '循环模式'];
 export function Home(props) {
   const {
     deviceData,
@@ -18,16 +16,24 @@ export function Home(props) {
 
   const countRef = useRef(null);
 
+  const [speed, setSpeed] = useState(0);
   const [modeVisible, setModeVisible] = useState(false);
-  const [modeValue, setModeValue] = useState(deviceData.working_mode || '');
+  const [modeValue, setModeValue] = useState(0);
 
+  useEffect(() => {
+    setModeValue(deviceData.working_mode || 0)
+  }, [deviceData.working_mode])
+
+  useEffect(() => {
+    setSpeed(deviceData.wind_speed || 0)
+  }, [deviceData.wind_speed])
 
   const handleToggle = (isAdd: boolean) => {
     if (deviceData.power_switch !== 1) return;
 
-    const action = 'humidity_set';
+    const action = 'wind_speed';
     const max = 100;
-    const oldVal = deviceData.humidity_set || 0;
+    const oldVal = deviceData.wind_speed || 0;
 
     if (isAdd) {
       if (oldVal < max) {
@@ -48,16 +54,11 @@ export function Home(props) {
     doControlDeviceData('power_switch', Number(!deviceData.power_switch));
   }
 
-  const onModeClick = () => {
-    setModeVisible(true);
+  const onModeClick = (value) => {
+    doControlDeviceData('working_mode', value);
   }
 
   const onCountDownClick = () => {
-    if (deviceData.count_down) {
-      push(PATH.TIMER_COUNTDOWNPAGE, { value: deviceData.count_down });
-      return;
-    }
-    debugger;
     countRef.current.onOpen();
   }
 
@@ -80,25 +81,16 @@ export function Home(props) {
         deviceData.power_switch === 1 ? 'power-on' : 'power-off',
       )}
     >
-      {/* 模式 */}
-      <div className="top">
-        <Battery
-          value={deviceData?.low_voltage?.voltage || 50}
-          isShowPercent={true}
-          isShowTip={false}
-        />
-        {/* <div className="mode-content">{MODE_LIST[deviceData?.working_mode || 0]}</div> */}
-      </div>
       {/* 表盘 */}
       <div className={classNames('disk-wrap')}>
         <div className="outer">
-          <div className="center">
-            <div className="inner">
-              <div className="title">目标湿度</div>
-              <div className="content">
-                <div className="value">{deviceData.humidity_set || 0}</div>
-                <div className="unit">%</div>
-              </div>
+
+        </div>
+        <div className="center">
+          <div className="inner">
+            <div className="title">风速</div>
+            <div className="content">
+              <div className="value">{deviceData.wind_speed || 0}</div>
             </div>
           </div>
         </div>
@@ -112,58 +104,86 @@ export function Home(props) {
           <div className="btn" onClick={onMinusClick}>
             <Icon name="minus" />
           </div>
+          <div className="custom-slider">
+            <div className="scale-list">
+              <div className='scale scale-1'></div>
+              <div className='scale scale-2'></div>
+              <div className='scale scale-3'></div>
+              <div className='scale scale-4'></div>
+              <div className='scale scale-5'></div>
+              <div className='scale scale-6'></div>
+              <div className='scale scale-7'></div>
+              <div className='scale scale-8'></div>
+              <div className='scale scale-9'></div>
+            </div>
+            <Slider
+              step={1}
+              min={0}
+              max={100}
+              value={speed}
+              // marks={marks}
+              onChange={(val) => {
+                setSpeed(val);
+                doControlDeviceData({
+                  wind_speed: val,
+                });
+              }}
+            >
+            </Slider>
+
+          </div>
           <div className="btn" onClick={onPlusClick}>
             <Icon name="plus" />
           </div>
         </div>
+
         <div className="actions">
           <Cell
-            className="cell-settings switch-btn-settings"
+            className="cell-first"
             title="开关"
             isLink={true}
             prefixIcon={<Icon name="switch" />}
             onClick={onSwitchClick}
           ></Cell>
           <Cell
-            className="cell-settings"
+            className="cell-first"
             title="定时"
             isLink={true}
             prefixIcon={<Icon name="time" />}
             onClick={onTimeClick}
-          ></Cell>
+          >
+          </Cell>
           <Cell
-            className="cell-settings"
+            className="cell-first"
             title="倒计时"
             isLink={true}
             prefixIcon={<Icon name="count" />}
             onClick={onCountDownClick}
-          >
-          </Cell>
+          ></Cell>
           <Cell
-            className="cell-settings mode-btn-settings"
+            className="cell-first"
             title="模式"
             isLink={true}
             prefixIcon={<Icon name="mode" />}
-            onClick={onModeClick}
+            onClick={() => { setModeVisible(true) }}
           >
-            <OptionDialog
-              visible={modeVisible}
-              title="模式"
-              defaultValue={[modeValue ? modeValue : 0]}
-              options={MODE_LIST.map((item, index) => ({ label: item, value: index }))}
-              onCancel={() => {
-                setModeVisible(false);
-              }}
-              onConfirm={(value) => {
-                setModeValue(value[0]);
-                doControlDeviceData('working_mode', value[0]);
-              }}
-            ></OptionDialog>
           </Cell>
         </div>
-        <CountDown ref={countRef} {...props} />
-
       </footer>
+      <OptionDialog
+        visible={modeVisible}
+        title="模式"
+        value={[modeValue || 0]}
+        options={MODE_LIST.map((item, index) => ({ label: item, value: index }))}
+        onCancel={() => {
+          setModeVisible(false);
+        }}
+        onConfirm={(value) => {
+          setModeValue(value[0]);
+          doControlDeviceData('working_mode', value[0]);
+        }}
+      ></OptionDialog>
+      <CountDown ref={countRef} {...props} isModule={true} />
     </main>
   );
 }
