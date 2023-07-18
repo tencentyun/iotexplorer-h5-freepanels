40dd8571687aa8f1a49c4ba30eb48e1cd3775630
chenwei9 <chenwei9@ruijie.com.cn>
2023-07-18 15:53:50 +0800

    主题联调

--- src/panels/ControlFourV3/Common/Theme/Theme.tsx
+++ src/panels/ControlFourV3/Common/Theme/Theme.tsx
@@ -8,10 +8,68 @@ import { Space, SpinLoading } from 'antd-mobile'
 
 export function Theme({ ...props }) {
   useTitle('主题风格');
-  const { doControlDeviceData, deviceData = {}, deviceInfo, themeList } = { ...props };
+  const { doControlDeviceData, deviceData = {}, deviceInfo } = { ...props };
   const { theme_style } = deviceData;
   const [loading, setLoading] = useState(false);
+  const [themeList, setThemeList] = useState([]);
 
+  const getThemeList = async () => {
+    const { Result } = await sdk.requestTokenApi('AppGetResourceThumbnailList', {
+      Action: 'AppGetResourceThumbnailList',
+      // AccessToken: 'AccessToken',
+      ProductId: deviceInfo.ProductId,
+      // FamilyId: sdk.familyId,
+      // RoomId: sdk.roomId,
+      Offset: 0,
+      Limit: 50
+    });
+    console.log('theme', Result);
+    // const result1 = [
+    //   {
+    //     "CreateTime": "2023-07-03 16:38:15",
+    //     "ProductName": "5uiy7bb6_test2023",
+    //     "ProductID": "0I2YXP9S11",
+    //     "Name": "测试",
+    //     "Md5": "9b5959e6147f4e15d5809eb4ef5ff678",
+    //     "Size": 2008054,
+    //     "Description": "缩略图",
+    //     "ResourceThumbnailInfo": [
+    //       {
+    //         "CosUrl": "https://iothub-tasktest-1256872341.cos.ap-guangzhou.myqcloud.com/600000559219/0I2YXP9S11_product/thumbnail/%E6%B5%8B%E8%AF%95_uncompress/6lIVr77uihU.jpg?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDQhstjLituhcKq9D2c096pJZaKsFl8PVV%26q-sign-time%3D1688440470%3B1688447670%26q-key-time%3D1688440470%3B1688447670%26q-header-list%3Dhost%26q-url-param-list%3D%26q-signature%3D7f57ca6761ffb3bcefb8fb78ba16f49ad852b54e",
+    //         "Name": "6lIVr77uihU.jpg"
+    //       },
+    //       {
+    //         "CosUrl": "https://iothub-tasktest-1256872341.cos.ap-guangzhou.myqcloud.com/600000559219/0I2YXP9S11_product/thumbnail/%E6%B5%8B%E8%AF%95_uncompress/9jFPVQzUgNY.jpg?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDQhstjLituhcKq9D2c096pJZaKsFl8PVV%26q-sign-time%3D1688440470%3B1688447670%26q-key-time%3D1688440470%3B1688447670%26q-header-list%3Dhost%26q-url-param-list%3D%26q-signature%3D7b402a94fe2bcb36cbc618fb09a046aedb7aabfa",
+    //         "Name": "9jFPVQzUgNY.jpg"
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     "CreateTime": "2023-07-03 17:02:10",
+    //     "ProductName": "5uiy7bb6_test2023",
+    //     "ProductID": "0I2YXP9S11",
+    //     "Name": "测试资源",
+    //     "Md5": "9b5959e6147f4e15d5809eb4ef5ff678",
+    //     "Size": 2008054,
+    //     "Description": "新",
+    //     "ResourceThumbnailInfo": [
+    //       {
+    //         "CosUrl": "https://iothub-tasktest-1256872341.cos.ap-guangzhou.myqcloud.com/600000559219/0I2YXP9S11_product/thumbnail/%E6%B5%8B%E8%AF%95_uncompress/6lIVr77uihU.jpg?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDQhstjLituhcKq9D2c096pJZaKsFl8PVV%26q-sign-time%3D1688440470%3B1688447670%26q-key-time%3D1688440470%3B1688447670%26q-header-list%3Dhost%26q-url-param-list%3D%26q-signature%3D7f57ca6761ffb3bcefb8fb78ba16f49ad852b54e",
+    //         "Name": "6lIVr77uihU.jpg"
+    //       },
+    //       {
+    //         "CosUrl": "https://iothub-tasktest-1256872341.cos.ap-guangzhou.myqcloud.com/600000559219/0I2YXP9S11_product/thumbnail/%E6%B5%8B%E8%AF%95_uncompress/9jFPVQzUgNY.jpg?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDQhstjLituhcKq9D2c096pJZaKsFl8PVV%26q-sign-time%3D1688440470%3B1688447670%26q-key-time%3D1688440470%3B1688447670%26q-header-list%3Dhost%26q-url-param-list%3D%26q-signature%3D7b402a94fe2bcb36cbc618fb09a046aedb7aabfa",
+    //         "Name": "9jFPVQzUgNY.jpg"
+    //       }
+    //     ]
+    //   }
+    // ];
+    setThemeList(Result);
+  }
+
+  useEffect(() => {
+    getThemeList()
+  }, [])
   return (
     <div className="theme-list">
       {themeList.map(({ Name, ResourceThumbnailInfo = [] }, index) => (
