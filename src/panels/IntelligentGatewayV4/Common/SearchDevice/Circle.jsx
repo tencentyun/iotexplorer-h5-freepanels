f239a5b906b805833e007ca8e8d39d8fe363d5ae
djdong <djdong@tencent.com>
2023-09-14 22:35:30 +0800

    feat(IntelligentGatewayV4): 精选网关面板优化

--- src/panels/IntelligentGatewayV4/Common/SearchDevice/Circle.jsx
+++ src/panels/IntelligentGatewayV4/Common/SearchDevice/Circle.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { Icon } from '@custom/Icon';
 
 
-export function Circle({ status, message = [], msg }) {
+export function Circle({ isActive, message = [], msg }) {
   return (
     <div className='circle'>
       <div className='round'>
         <div className='label'>
-          <div className={`dot ${status ? 'enable' : 'disabled'}`}></div>
+          <div className={`dot ${isActive ? 'enable' : 'disabled'}`}></div>
           <div className='msg'>{msg}</div>
         </div>
         <div className='flag'>
-          <div className={`my-flag ${status ? 'start' : 'stop'}`}>
+          <div className={`my-flag ${isActive ? 'start' : 'stop'}`}>
             <Icon name='flag-i'></Icon>
           </div>
         </div>
       </div>
       <div className='message'>{
-        message.map(val => <div>{val}</div>)
+        message.map((val, index) => <div key={index}>{val}</div>)
       }</div>
     </div>
   );
