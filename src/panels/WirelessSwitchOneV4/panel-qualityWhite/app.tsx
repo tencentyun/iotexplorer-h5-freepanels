1c33ee61df61909504e94e48feb9c54a4a27a8ae
cloverwwang <cloverwwang@tencent.com>
2024-10-10 11:12:38 +0800

    Revert "feat(ControlFourV3): 更新智控屏翻译"
    This reverts commit 4799d93890bd15fe49f4e16c1dfa21a54e973c83.

--- src/panels/WirelessSwitchOneV4/panel-qualityWhite/app.tsx
+++ src/panels/WirelessSwitchOneV4/panel-qualityWhite/app.tsx
@@ -2,4 +2,5 @@ import render from '@custom/Render';
 import App from '../Common/Common';
 import '@theme/panel-qualityWhite';
 import './app.less';
-render(App, { switchNum: 1, sceneBtnModeDesc: ['单击时', '双击时', '长按时'] });
+import { t } from '@locales';
+render(App, { switchNum: 1, sceneBtnModeDesc: [t('单击时'), t('双击时'), t('长按时')] });
